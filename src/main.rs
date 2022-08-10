e4213f9be00c08e6b2cbb7e90cbfafea7e42619d
Tyler Seabury <tylerseabury@gmail.com>
2022-08-10 17:06:16 -0400

    added clap for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-use std::env;
+use clap::Parser;
+
+/// A simple CLI utility for batch renaming.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The pattern
+    #[clap(short, long, value_parser)]
+    pattern: String,
+
+    /// The prefix
+    #[clap(short, long, value_parser)]
+    prefix: String,
+}
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() > 1 {
-        let word: &String = &args[1];
-        let upper: String = word.to_uppercase();
-        println!("{:?}", upper);
-    }
+    let args = Args::parse();
+
+    println!("{:?}", to_uppercase(args.pattern));
+    println!("{:?}", to_uppercase(args.prefix));
+}
+
+fn to_uppercase(word: String) -> String {
+    let upper: String = word.to_uppercase();
+    upper
 }
